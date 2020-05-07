e24b70f562205af591e0b224464cadc860a04763
Miha Friškovec <friskovec.miha@gmail.com>
2020-05-07 16:09:10 +0200

    wip

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+as
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: FlatButton(
+              child: Text("Click me"),
+              onPressed: () {
+                final player = AudioCache();
+
+                player.play("note1.wav");
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
